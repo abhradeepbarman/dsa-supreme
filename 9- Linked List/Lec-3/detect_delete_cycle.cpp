6d681725f1bf38d74540062fd9ee8022fcfaacb0
abhradeepbarman <abhradeep709@gmail.com>
2023-08-04 22:49:30 +0530

    LL lec-3 added

---
+++ 9- Linked List/Lec-3/detect_delete_cycle.cpp
@@ -0,0 +1,174 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+//check if loop is present or not
+bool checkForLoop(Node* &head) {
+    
+    if(head == NULL) {
+        //empty LL
+        return false;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+
+    while(fast != NULL) {
+        fast = fast->next;
+        if(fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        if(slow == fast) {
+            //loop present
+            return true;
+        }
+    }
+    //fast == NULL --> loop not present
+    return false;
+}
+
+//find the starting point of the loop
+Node* startingPointLoop(Node* &head) {
+    
+    if(head == NULL) {
+        //empty LL
+        return head;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+
+    while(fast != NULL) {
+        fast = fast->next;
+        if(fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        if(slow == fast) {
+            slow = head;
+            break;
+        }
+    }
+
+    while(slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    //starting point
+    return slow;  //we can return fast also
+}
+
+//remove the loop
+Node* deleteLoop(Node* &head) {
+    
+    if(head == NULL) {
+        //empty LL
+        return head;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+
+    while(fast != NULL) {
+        fast = fast->next;
+        if(fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        if(slow == fast) {
+            slow = head;
+            break;
+        }
+    }
+
+    while(slow->next != fast->next) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    
+    //delete loop
+    fast->next = NULL;
+    return head;
+}
+
+void print(Node* head) {
+
+    Node* temp = head;
+
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(50);
+    Node* sixth = new Node(60);
+    Node* seventh = new Node(70);
+    Node* eighth = new Node(80);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
+    seventh->next = eighth;
+    eighth->next = fifth;
+
+    //check if loop is present or not
+    bool ans = checkForLoop(head);
+
+    if(ans) {
+        cout << "Loop present" << endl;
+    } else {
+        cout << "Loop absent" << endl;
+    }
+
+    //find the starting point of sloop
+    Node* start = startingPointLoop(head);
+
+    cout << "starting Node : " << start->data << endl;
+
+
+    //delete loop
+    deleteLoop(head);
+    print(head);
+
+
+    //check if loop is present or not
+    ans = checkForLoop(head);
+
+    if(ans) {
+        cout << "Loop present" << endl;
+    } else {
+        cout << "Loop absent" << endl;
+    }
+
+
+    return 0;
+}
---
+++ 9- Linked List/Lec-3/k-groups_reverse.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node* head) {
+
+    Node* temp = head;
+
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int getLength(Node* &head) {
+    int len = 0;
+    Node* temp = head;
+
+    while(temp != NULL) {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
+Node* reverseKNodes(Node* &head, int k)
+{
+    if(head == NULL) {
+        //empty LL
+        return NULL;
+    }
+
+    int len = getLength(head);
+    if(k > len) {
+        //invalid value for k
+        return head;
+    }
+
+    //it means no. of nodes in LL is >= k
+    //Step 1: reverse first k nodes of LL
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = curr->next;
+
+    int count = 0;
+
+    while(count < k) {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+        count++;
+    }
+
+    //Step 2: recursive call
+    if(forward != NULL) {
+        //we still have nodes left to reverse
+        head->next = reverseKNodes(forward, k);
+    }
+
+    //Step 3: return head of the modified LL
+    return prev;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(50);
+    Node* sixth = new Node(60);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+
+    print(head);
+
+    head = reverseKNodes(head, 3);
+
+    print(head);
+    
+    return 0;
+}
